a3b1035a43179a378c4121fdd5cb2250b52372c5
Home <omskathletics@mail.ru>
2018-08-30 18:07:31 +0600

    With threads anq queue

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ import socket
 import json
 import os
 import re
+import queue
+import time
+
+q = queue.Queue()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
 
 connections = set() #set of websocket "clients"
 
+s = socket.socket()
+s.bind(('', 6100))
+print('binded')
+s.listen(1)
+print('listened')
 
-@gen.coroutine
-def getData():
-    # In real world data comes from socket
-    #
-    s = socket.socket()
-    s.bind(('', 6100))
-    print('binded')
-    s.listen(1)
-    print('listened')
+#@gen.coroutine
+def getData(q):
     conn, addr = s.accept()
     print('accepted')
 
@@ -29,7 +31,7 @@ def getData():
     pattern_res = '\d\d?:\d\d\.\d\d\d|\d\d?\.\d\d\d' 
     key = 'default'
     while True:
-        yield gen.sleep(0.000001)  #this prevent blocking and allow other client to connect
+        #yield gen.sleep(0.000001)  #this prevent blocking and allow other client to connect
         data = conn.recv(1024)
         if not data:
             break
@@ -41,7 +43,6 @@ def getData():
         except:
             data = []
         for p_data in data:
-            print(data)
             try:
                 res = re.findall(pattern_res, p_data)[0]
                 res = res.replace('.', ',')
@@ -49,19 +50,30 @@ def getData():
                 res = 'nothing'
             if res:
                 l_data = p_data.split('|')
-                ab = l_data[2].strip()
+                ab = l_data[0].strip()
+                cd = l_data[1].strip()
+                ee = l_data[2].strip()
                 bib = l_data[4].strip()
                 pulse = l_data[3].strip()
                 if bib == '0':
                     pulse = 0
-                    key = '{}-{}'.format(res, ab)
+                    key = '{}-{}-{}-{}'.format(res, ab, cd, ee)
                     res = 'start'
                 results = {'key': key, 'res': res, 'bib': bib, 'pulse': pulse}
                 m_dict = { 'action': 'result', 'result': results }
-                msg = json.dumps(m_dict)
-                [con.write_message(msg) for con in connections]
+                q.put(m_dict)
 
+    getData(q)
 
+def sendToAll(q):
+    while True:
+        while not q.empty():
+            m_dict = q.get()
+            msg = json.dumps(m_dict)
+            print(connections)
+            print(msg)
+            [con.write_message(msg) for con in connections]
+        time.sleep(0.5)
 
 class WebSocketHandler(WebSocketHandler):
 
@@ -106,5 +118,14 @@ if __name__ == '__main__':
     ws_app = Application()
     server = tornado.httpserver.HTTPServer(ws_app)
     server.listen(8080)
-    IOLoop.current().spawn_callback(getData)
-    IOLoop.instance().start()
+    #IOLoop.current().spawn_callback(getData)
+
+    from threading import Thread
+    workers = [
+        Thread(target=getData, args=(q,)),
+        Thread(target=sendToAll, args=(q,)),
+    ]
+    for w in workers:
+        w.daemon = True
+        w.start()
+    IOLoop.current().start()
