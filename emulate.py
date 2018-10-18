01e318ba01a1f783fef1d79e9c6b66be65b7ee4f
Home <omskathletics@mail.ru>
2018-10-18 14:14:12 +0600

    Remains laps

--- emulate.py
+++ emulate.py
@@ -5,9 +5,9 @@ import sys
 s = socket.socket()
 s.connect(('', 6100))
 
-with open('out5.txt', 'rb') as f:
+with open('out6.txt', 'rb') as f:
     for line in f:
-        time.sleep(0.05)
+        time.sleep(0.09)
         s.send(line)
         print(line.decode())
 
