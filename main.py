2ceec61e7f93f2c002429bd7e6eea1baa37e3fd5
la55 <omskathletics@mail.ru>
2019-03-28 18:20:18 +0600

    Save to db

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ import os
 import re
 import queue
 import time
+import sqlite3
+
 
 define("host", default="localhost", help="app host", type=str)
 define("port", default=8080, help="app port", type=int)
 
+dbconn = sqlite3.connect('db.sqlite3')
+cursor = dbconn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS results (key INTEGER, result TEXT, bib INTEGER, pulse INTEGER)''')
+dbconn.commit()
+
 q = queue.Queue()
+q_res = queue.Queue()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
 
@@ -46,15 +55,41 @@ def getData():
                 res = res_match.group().replace('.', ',')
                 ab, cd, ee, pulse, bib, *xx = p_data.split('|')
                 if len(xx) < 2:
+                    ab, cd, ee, bib1, bib2, xx = p_data.split('|')
                     tmpst = time.time()
+                    key1 = '{}-{}'.format(tmpst, bib1)
+                    key2 = '{}-{}'.format(tmpst, bib2)
+                    result1 = {'key': key1, 'res': 'A', 'bib': bib1, 'pulse': pulse}
+                    result2 = {'key': key2, 'res': 'B', 'bib': bib2, 'pulse': pulse}
+                    for result in [result1, result2]:
+                        m_dict = { 'action': 'result', 'result': result }
+                        q.put(m_dict)
                     continue
                 key = '{}-{}'.format(tmpst, bib)
-                results = {'key': key, 'res': res, 'bib': bib, 'pulse': pulse}
-                m_dict = { 'action': 'result', 'result': results }
+                result = {'key': key, 'res': res, 'bib': bib, 'pulse': pulse}
+                m_dict = { 'action': 'result', 'result': result }
                 q.put(m_dict)
+                res_db = {'key': tmpst, 'res': res, 'bib': bib, 'pulse': pulse}
+                res_dict = { 'action': 'result', 'result': res_db }
+                q_res.put(res_dict)
                 print("Thread 1: pull data from socket: {} - {}".format(pulse, res))
 
     getData()
+
+
+def saveRes():
+    dbconn = sqlite3.connect('db.sqlite3')
+    cursor = dbconn.cursor()
+    while True:
+        while not q.empty():
+            res = q_res.get()
+            if res['action'] == 'result':
+                key, res, bib, pulse = res['result'].values()
+                print('Values: ', key, res, bib, pulse)
+                sql = '''INSERT INTO results VALUES ({}, '{}', {}, {}) '''.format(key, res, bib, pulse)
+                cursor.execute(sql)
+                dbconn.commit()
+
 
 #@gen.coroutine
 def sendToAll():
@@ -79,7 +114,16 @@ class WebSocketHandler(WebSocketHandler):
             connections.add(self)
 
     def on_message(self, message):
-        pass #not used
+        data = json.loads(message)
+        print(data)
+        if data['action'] == 'edit_bib':
+            bib = data['bib']
+            key = data['key'].split('-')[0]
+            sql = '''UPDATE results SET bib={} WHERE key={}'''.format(bib, key)
+            cursor.execute(sql)
+            dbconn.commit()
+            msg = json.dumps({'action': 'edit_bib', 'bib': bib, 'key': key })
+            [(con.write_message(msg), print("Bib updated: {}".format(bib))) for con in connections]
 
     def on_close(self):
         connections.remove(self)
@@ -121,6 +165,7 @@ if __name__ == '__main__':
     workers = [
         Thread(target=getData),
         Thread(target=sendToAll),
+        Thread(target=saveRes),
     ]
     for w in workers:
         w.daemon = True
